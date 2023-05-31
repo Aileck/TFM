e89260aceca4a4cf17a8d0de8ff32024bca14b6e
Aileck <61742408+Aileck@users.noreply.github.com>
2023-05-31 21:42:21 +0200

    update scene

--- Assets/VRMovment.cs
+++ Assets/VRMovment.cs
@@ -5,8 +5,9 @@ using Valve.VR;
 
 public class VRMovment : MonoBehaviour
 {
-   
+
     // Movment
+    public bool KeyboardMode;
     public Transform vrCamera;
  
     public SteamVR_Action_Vector2 touchpadAction;
@@ -15,6 +16,7 @@ public class VRMovment : MonoBehaviour
     public float trackpadSensitivity = 1.0f;
 
     public ForceMode movmentMode;
+    public float rotationY = 0f;
 
     //Rotation
     //private SteamVR_TrackedObject trackedObj;
@@ -22,45 +24,45 @@ public class VRMovment : MonoBehaviour
 
     private void Start()
     {
+        if (KeyboardMode) {
+            Cursor.lockState = CursorLockMode.Locked;
+
+            this.transform.position = new Vector3(this.transform.position.x,
+                                                  1,
+                                                  this.transform.position.z);
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+        }
     }
     // Actualizamos el script en cada fotograma
     void Update()
     {
+  
         Movment();
         Rotation();
+
+        if (KeyboardMode)
+        {
+            KeyboardMovment();
+            KeyboardRotation();
+        }
 
     }
 
     private void Movment() {
-        // Comprobamos si el touchpad derecho ha sido pulsado
+        // Comprobamos si el touchpad ha sido pulsado
         Vector2 touchpadValue = touchpadAction.GetAxis(SteamVR_Input_Sources.Any);
 
         if (touchpadValue != Vector2.zero)
         {
             Vector3 cameraPosition = vrCamera.position;
 
-            // Obtenemos el vector de entrada del touchpad del controlador derecho
-
-            // Escalamos la entrada del touchpad por la sensibilidad del touchpad
             touchpadValue *= trackpadSensitivity;
-
-            // Movemos la cámara VR en la dirección de la entrada del touchpad
-            //cameraPosition += vrCamera.transform.forward * touchpadValue.y * movementSpeed;
-            //cameraPosition += vrCamera.transform.right * touchpadValue.x * movementSpeed;
-
-            //// Establecemos la nueva posición de la cámara VR
-            //vrCamera.position = cameraPosition;
 
             Vector3 forceDirection = vrCamera.transform.forward * touchpadValue.y + vrCamera.transform.right * touchpadValue.x;
             forceDirection.Normalize();
 
-            //Debug.Log(forceDirection);
-
             // Aplicamos la fuerza en la dirección calculada
             this.GetComponent<Rigidbody>().AddForce(forceDirection * movementSpeed, movmentMode);
-
-            //this.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
-
 
         }
     }
@@ -68,6 +70,36 @@ public class VRMovment : MonoBehaviour
 
     private void Rotation() {
 
+    }
+
+    private void KeyboardMovment()
+    {
+        float touchpadX = Input.GetAxis("Horizontal");
+        float touchpadY = Input.GetAxis("Vertical");
+        Vector2 touchpadValue = new Vector2(touchpadX, touchpadY);
+
+        Debug.Log(touchpadValue);
+        if (touchpadValue != Vector2.zero)
+        {
+            Vector3 cameraPosition = vrCamera.position;
+
+            touchpadValue *= trackpadSensitivity*0.07f;
+
+            Vector3 forceDirection = vrCamera.transform.forward * touchpadValue.y + vrCamera.transform.right * touchpadValue.x;
+            //forceDirection.Normalize();
+
+            GetComponent<Rigidbody>().AddForce(forceDirection * movementSpeed, movmentMode);
+        }
+    }
+
+    private void KeyboardRotation() {
+        float mouseX = Input.GetAxis("Mouse X") * trackpadSensitivity * 100 * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * trackpadSensitivity * Time.deltaTime;
+
+        rotationY -= mouseX;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(0f, -rotationY, 0f);
     }
 
     private void OnTriggerEnter(Collider other)
